7e3ce4e5ea6b3e8801665e4fb9cb017685633451
PM <121531128+26-PM@users.noreply.github.com>
2024-10-10 18:06:10 +0000

    time stamp added

--- models/ReportModel.js
+++ models/ReportModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const reportSchema = new mongoose.Schema({
   filesArray: [String],
   location: { latitude: String, longitude: String },
-});
+}, { timestamps: true }); // Enable timestamps
 
 const reportModel = mongoose.model("Report", reportSchema);
 
