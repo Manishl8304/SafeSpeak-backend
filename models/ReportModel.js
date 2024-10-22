7fa003ce8ffe40cb83c889390519c30498fd909a
Manish <manishaggarwal8304@gmail.com>
2024-10-23 02:09:49 +0530

    added category

--- models/ReportModel.js
+++ models/ReportModel.js
@@ -5,6 +5,7 @@ const reportSchema = new mongoose.Schema(
     filesArray: [String],
     location: { latitude: String, longitude: String },
     description: String,
+    category: String,
   },
   { timestamps: true }
 ); // Enable timestamps
