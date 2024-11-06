facf26cc97093dfe7b3ebd2f998b0bf24db12c8a
Manish <manishaggarwal8304@gmail.com>
2024-11-06 18:40:23 +0530

    xyz

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 app.use(
   cors({
-    // origin: ["http://localhost:5173", "https://safe-speak-xp7j.vercel.app/"],
+    origin: ["http://localhost:5173", "https://safe-speak-xp7j.vercel.app"],
     credentials: true,
   })
 );
