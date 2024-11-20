2448d61fea07532078b064c40ed101c2eb188a72
PM <121531128+26-PM@users.noreply.github.com>
2024-11-20 14:19:32 +0000

    check

--- app.js
+++ app.js
@@ -26,10 +26,7 @@ app.use((req, res, next) => {
 });
 
 app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://safe-speak-smoky.vercel.app"],
-    credentials: true,
-  })
+  cors()
 );
 app.use(express.json());
 app.use(cookieParser());
